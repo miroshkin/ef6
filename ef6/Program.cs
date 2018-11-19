27369264b9b8d2dac82226678d2555b73ac3b447
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-11-19 12:02:07 +0300

    Added sorting

--- ef6/Program.cs
+++ ef6/Program.cs
@@ -17,11 +17,23 @@ namespace ef6
 
                 //var comicBooks = context.ComicBooks.ToList();
 
-                var comicBooksQuery = from cb in context.ComicBooks select cb;
-                var comicBooks = comicBooksQuery
+                //var comicBooksQuery = from cb in context.ComicBooks select cb;
+                //var comicBooks = comicBooksQuery
+                //    .Include(cb => cb.Series)
+                //    .OrderByDescending(cb => cb.IssueNumber)
+                //    .ThenBy(cb => cb.PublishedOn)
+                //    .ToList();
+
+                var comicBooksQuery = context.ComicBooks
                     .Include(cb => cb.Series)
-                    .Where(cb => cb.Series.Title.Contains("man"))
+                    .OrderByDescending(cb => cb.IssueNumber);
+
+                var comicBooks = comicBooksQuery.ToList();
+
+                var comicBooks2 = comicBooksQuery
+                    .Where(cb => cb.AverageRating < 7)
                     .ToList();
+                    
 
                 foreach (var comicBook in comicBooks)
                 {
@@ -30,7 +42,15 @@ namespace ef6
 
                 Console.WriteLine();
                 Console.WriteLine("# of comic books {0}", comicBooks.Count);
+                Console.WriteLine();
 
+                foreach (var comicBook in comicBooks2)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("# of comic books {0}", comicBooks2.Count);
 
                 //var books = context.ComicBooks
                 //    //.Include(cb => cb.Series)
